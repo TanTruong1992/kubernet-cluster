f6dc79e7fddaeebd0b90383026ea69e4848d271b
TanTruong1992 <50537207+TanTruong1992@users.noreply.github.com>
2025-04-06 13:29:37 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 # VPC Module
 module "vpc" {
   source              = "./modules/vpc"
